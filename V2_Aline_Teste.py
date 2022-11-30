46495f0cd519550b5d92c00a68c660122f757bb0
Aline Henrique Alves Barros <89356097+AlineHAB@users.noreply.github.com>
2022-11-30 17:37:55 -0300

    Update V2_Aline_Teste.py

--- V2_Aline_Teste.py
+++ V2_Aline_Teste.py
@@ -3,7 +3,7 @@ def viagem_volta():
     percurso_total =  130         #305.779  
     galoes = 15.14                #4 * (3,78544 = 1 galão americano)
     km_sol =  10                  #23.5215            
-    km_chuva =   7                #716.465          
+    km_chuva = 7                #716.465          
     
     volta_no_sol = (km_sol * galoes)       #km_sol * 3,7 = resultado -> resultado * 4 
     volta_na_chuva = (km_chuva * galoes)
