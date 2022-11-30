51408d9bb9f9464903f36db1dc78b3748923a49e
Aline Henrique Alves Barros <89356097+AlineHAB@users.noreply.github.com>
2022-11-30 17:35:38 -0300

    Add files via upload

---
+++ V2_Aline_Teste.py
@@ -0,0 +1,28 @@
+def viagem_volta():
+
+    percurso_total =  130         #305.779  
+    galoes = 15.14                #4 * (3,78544 = 1 galão americano)
+    km_sol =  10                  #23.5215            
+    km_chuva =   7                #716.465          
+    
+    volta_no_sol = (km_sol * galoes)       #km_sol * 3,7 = resultado -> resultado * 4 
+    volta_na_chuva = (km_chuva * galoes)
+
+    if volta_no_sol:
+        if volta_no_sol > percurso_total:
+            print('Dias de Sol')
+            return True
+        else:
+            print('Dias de Sol')
+            return False
+
+    elif volta_na_chuva:
+        if volta_na_chuva > percurso_total:
+            print('Dias de Chuva')
+            return True
+        else:
+            print('Dias de Chuva')
+            return False
+
+print(viagem_volta())
+
